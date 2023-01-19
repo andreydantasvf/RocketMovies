9c803d5efcba2ae3c5d2e40b438f74e1461b3ce4
Andrey Dantas <andreydantasvf@gmail.com>
2023-01-19 13:43:18 -0300

    Backend

--- web/src/components/RatingMovie/styles.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.div`
-    > .css-ryrseu-MuiRating-root, .MuiRating-iconEmpty {
-        color: ${({theme}) => theme.COLORS.PINK};
-    }
-`;
