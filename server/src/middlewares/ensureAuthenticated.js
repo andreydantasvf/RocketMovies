9521ff547a454a7c412b9561953f9362edf03d86
Andrey Dantas <andreydantasvf@gmail.com>
2023-01-19 15:23:23 -0300

    Criando função middleware para verificar se existe um token no usuario

--- server/src/middlewares/ensureAuthenticated.js
+++ server/src/middlewares/ensureAuthenticated.js
@@ -3,26 +3,25 @@ const AppError = require("../utils/AppError");
 const authConfig = require("../configs/auth");
 
 function ensureAuthenticated(request, response, next) {
-  // capturando o Token JWT do request.header
-  const authHeader = request.headers.authorization;
+    const authHeader = request.headers.authorization
 
-  if (!authHeader) {
-    throw new AppError("JWT Token não imformado", 401);
-  }
-
-  const [_, token] = authHeader.split(" ");
-
-  try {
-    const { sub: user_id } = verify(token, authConfig.jwt.secret);
-    
-    request.user = {
-      id: Number(user_id),
+    if (!authHeader) {
+        throw new AppError("JWT Token não informado", 401)
     }
 
-    return next();
-  } catch {
-    throw new AppError("JWT Token inválido", 401);
-  }
+    const [, token] = authHeader.split(" ")
+
+    try {
+        const { sub: user_id } = verify(token, authConfig.jwt.secret)
+
+        request.user = {
+            id: Number(user_id)
+        }
+
+        return next()
+    } catch {
+        throw new AppError("JWT Token inválido", 401)
+    }
 }
 
 module.exports = ensureAuthenticated;
