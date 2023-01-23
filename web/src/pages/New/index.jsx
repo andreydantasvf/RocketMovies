273c0da2b435968b09cda03df73b801c77b44532
Andrey Dantas <andreydantasvf@gmail.com>
2023-01-23 15:10:42 -0300

    Função para adicionar um filme no banco de dados

--- web/src/pages/New/index.jsx
+++ web/src/pages/New/index.jsx
@@ -1,3 +1,8 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { api } from "../../services/api";
+
 import { Container, Form } from "./styles";
 
 import { Header } from '../../components/Header';
@@ -7,6 +12,59 @@ import { Input } from '../../components/Input';
 import { MovieItem } from '../../components/MovieItem';
 
 export function New() {
+    const [title, setTitle] = useState("");
+    const [rating, setRating] = useState("");
+    const [description, setDescription] = useState("");
+
+    const [tags, setTags] = useState([]);
+    const [newTag, setNewTag] = useState("");
+
+    const navigate = useNavigate();
+
+    function handleAddTag() {
+        if (tags.includes(newTag)) {
+            return alert("Essa tag já foi adicionada!");
+        }
+        setTags(prevState => [...prevState, newTag]);
+        setNewTag("");
+    }
+
+    function handleRemoveTag(deleted) {
+        setTags(prevState => prevState.filter(tag => tag !== deleted));
+    }
+
+    async function handleNewMovie() {
+        if (!title) {
+            return alert("Digite o título do filme.");
+        }
+
+        if (!rating) {
+            return alert("Digite a nota do filme.");
+        }
+
+        if (rating < 0 || rating > 5) {
+            return alert("Insira uma nota entre 0 e 5.");
+        }
+
+        if (!description) {
+            return alert("Digite a descrição do filme.");
+        }
+
+        if (newTag) {
+            return alert("Você deixou uma tag no campo para adicionar, mas não clicou em adicionar. Clique para adicionar ou deixe o campo vazio.")
+        }
+
+        await api.post("/movie", {
+            title,
+            description,
+            rating,
+            tags
+        })
+
+        alert("Filme adicionado com sucesso!");
+        navigate(-1);
+    }
+
     return (
         <Container>
             <Header />
@@ -18,21 +76,51 @@ export function New() {
                     <h1>Novo filme</h1>
 
                     <div className="inputs">
-                        <Input type="text" placeholder="Título" />
-                        <Input type="number" placeholder="Sua nota (de 0 a 5)" />
+                        <Input
+                            type="text"
+                            placeholder="Título"
+                            onChange={e => setTitle(e.target.value)}
+                        />
+                        <Input
+                            type="number"
+                            placeholder="Sua nota (de 0 a 5)"
+                            onChange={e => setRating(e.target.value)}
+                        />
                     </div>
-                    <textarea name="description" id="description" placeholder="Descrição"></textarea>
+                    <textarea
+                        name="description"
+                        id="description"
+                        placeholder="Descrição"
+                        onChange={e => setDescription(e.target.value)}
+                    />
 
                     <section>
                         Macadores
 
                         <div className="tags">
-                            <MovieItem value="React" />
-                            <MovieItem isNew placeholder="Novo marcador" />
+                            {
+                                tags.map((tag, index) => (
+                                    <MovieItem
+                                        key={String(index)}
+                                        value={tag}
+                                        onClick={() => handleRemoveTag(tag)}
+                                    />
+                                ))
+                            }
+                            <MovieItem
+                                isNew
+                                placeholder="Nova tag"
+                                value={newTag}
+                                onChange={e => setNewTag(e.target.value)}
+                                onClick={handleAddTag}
+                            />
                         </div>
                     </section>
 
-                    <Button title="Salvar" />
+                    <Button 
+                    title="Salvar" 
+                    onClick={handleNewMovie}
+                    />
                 </Form>
             </main>
         </Container>
