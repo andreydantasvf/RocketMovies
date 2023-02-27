c029b53ee6833c2a6450585710ea98bd6fcfda4a
Andrey Dantas <andreydantasvf@gmail.com>
2023-02-27 14:00:43 -0300

    adicionando as migrations na primeira vez que rodar o server

--- server/src/database/knex/index.js
+++ server/src/database/knex/index.js
@@ -2,5 +2,6 @@ const config = require("../../../knexfile");
 const knex = require("knex");
 
 const connection = knex(config.development)
+connection.migrate.latest([config.development]);
 
 module.exports = connection
